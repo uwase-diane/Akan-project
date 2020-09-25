837939b2851a65d2487e9f365d25a681c9eb0cff
uwase-diane <diane.uwase13@gmail.com>
2020-09-25 10:59:19 -0400

    created the form

---
+++ index.js
@@ -0,0 +1,10 @@
+// function callingNames(){
+//     var centries = document.getElementById("centry").value 
+//     var years = document.getElementById("year").value
+//     var months = document.getElementById("month").value
+//     var days = document.getElementById("day").value
+
+
+   
+// }
+
