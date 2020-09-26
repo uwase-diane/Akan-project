1769164e69500003c8a0ff8b654a207345cd6871
uwase-diane <diane.uwase13@gmail.com>
2020-09-26 04:55:13 -0400

    calculated the day value

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-// function callingNames(){
-//     var centries = document.getElementById("centry").value 
-//     var years = document.getElementById("year").value
-//     var months = document.getElementById("month").value
-//     var days = document.getElementById("day").value
+
+var result;
+var dd;
+var mm;
+var shortYear;
+var shortCentry;
 
 
-   
-// }
+function calculateAkanDay(){
 
+year = document.getElementById("year").value;
+shortYear = year.substr(2);
+shortCentry = year.substr(0, 2);
+mm = document.getElementById("month").value;
+dd = document.getElementById("date").value;
+
+result = (((shortCentry/4) -2*shortCentry-1) + ((5 * shortYear/4)) + (26 * (mm + 1)/10) + dd) % 7;
+console.log(result);
+document.getElementById("demo").innerHTML = alert("your akan day is " +(Math.floor(result)));
+
+
+}  
+
+
+
+  
